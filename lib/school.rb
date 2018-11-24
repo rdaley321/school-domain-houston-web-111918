766a96320b2348e735138d062cf935caebb897c3
Ryan Daley <rdaley321@yahoo.com>
2018-11-24 02:37:08 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name,grade)
+    self.roster[grade] = []
+    self.roster[grade] << name
+  end
+end
